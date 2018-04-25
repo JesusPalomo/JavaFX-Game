f86638abd020dbcfeec2e852f12b19d13ad0d4f0
jp08049 <jp08049@CEIT2551202X041.ad.georgiasouthern.edu>
2018-04-24 22:28:08 -0400

    thirdv2

--- BoardGame/src/NewGame.java
+++ BoardGame/src/NewGame.java
@@ -1,14 +1,15 @@
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.Scene;
 
 public class NewGame extends MainMenu {
 
 	public NewGame() {
-		System.out.print("New Game");
+		BorderPane pane = new BorderPane();
+		Label label = new Label("You went to the new game screen!");
+		pane.setCenter(label);
+		pane.getChildren().add(label);
+//		Scene newGameScreen = new Scene(pane, 750, 750);
 	}
-	
-//	HBox pane = new HBox();
-//	Label label = new Label("You went to the new game screen!");
-//	pane.getChildren().add(label);
 	
 }
