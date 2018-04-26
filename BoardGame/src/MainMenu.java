b7540000125975f4e26c31446ed7ed24b1245362
jp08049 <jp08049@CEIT2551202X041.ad.georgiasouthern.edu>
2018-04-25 22:22:03 -0400

    quik maffs

--- BoardGame/src/MainMenu.java
+++ BoardGame/src/MainMenu.java
@@ -10,11 +10,13 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 import javafx.geometry.*;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
 
-public class MainMenu extends Application implements EventHandler<ActionEvent> {
+public class MainMenu extends Application { 
 
+	private Stage gameStage = new Stage();
+	
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
@@ -28,7 +30,7 @@ public class MainMenu extends Application implements EventHandler<ActionEvent> {
 		ImageView logo = new ImageView(new Image("file:onitama.jpg"));
 		menu.getChildren().addAll(logo, newGame, tutorial);
 		
-		//Tutorial Screen
+		//Tutorial screen
 		StackPane instruct = new StackPane();
 		Label instructions = new Label("Put instructions here");
 		instruct.getChildren().add(instructions);
@@ -38,31 +40,38 @@ public class MainMenu extends Application implements EventHandler<ActionEvent> {
 		Button goBack = new Button("Return");
 		Button next = new Button("Next");
 		Button prev = new Button("Previous");
-		buttons.getChildren().addAll(goBack, next, prev);
+		buttons.getChildren().addAll(goBack, prev, next);
 		BorderPane tutMenu = new BorderPane();
 		tutMenu.setCenter(instruct);
 		tutMenu.setBottom(buttons);
 		
+		//New game screen
+		StackPane pane = new StackPane();
+		Label label = new Label("This is a new game!");
+		pane.getChildren().add(label);
+		
 		//Game Screens
 		Scene mainScreen = new Scene(menu, 750, 750);
 		Scene tutorialScreen = new Scene(tutMenu, 750, 750);
+		Scene gameScreen = new Scene(pane, 750, 750);
 		
 		//Button actions
 		tutorial.setOnAction(e -> primaryStage.setScene(tutorialScreen));
 		goBack.setOnAction(e -> primaryStage.setScene(mainScreen));
-		newGame.setOnAction(e -> new NewGame());
+		newGame.setOnAction(e -> {
+				gameStage.setScene(gameScreen);
+				gameStage.setTitle("Onitama Game");
+				gameStage.setMinHeight(750);
+				gameStage.setMinWidth(750);
+				gameStage.show();
+		});
 		
-		//Primary Stage
+		//Primary stage
 		primaryStage.setScene(mainScreen);
 		primaryStage.setTitle("Onitama");
 		primaryStage.setMinHeight(500);
 		primaryStage.setMinWidth(500);
 		primaryStage.show();
-	}
-
-	@Override
-	public void handle(ActionEvent event) {
-		
 	}
 
 }
